9b9098c4b10c67ef19cf85fb7c7bb651b6697c25
gigi <lpgpirisi@gmail.com>
2020-03-16 02:54:57 +0100

    some comments

--- TripAdvisorReviewScraper.py
+++ TripAdvisorReviewScraper.py
@@ -19,6 +19,7 @@ nReviews = int(nReviews.text.split(' ', 2)[0].replace('.', '', 6))
 avgMark = driver.find_element_by_class_name('flights-airline-review-page-overview-module-OverviewModule__overall_rating--30Bld').text
 avgMark = float(avgMark.split('\n', 2)[0].replace(',', '.', 5))
 
+#there will be 16 boxes bcause this class references to a box that is common for reviews, photos and suggestions tabs. we care only about the first 5 indices of the following list
 reviewBoxes = driver.find_elements_by_xpath('//div[@class="location-review-card-Card__ui_card--2Mri0 location-review-card-Card__card--o3LVm location-review-card-Card__section--NiAcw"]')
 
 print(companyName)
